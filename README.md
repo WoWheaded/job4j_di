07cc06ef3851a2e4db0b486f2997d96d37629693
WoWheaded <d-343@yandex.ru>
2024-01-08 22:24:52 +0300

    0. Что такое DI? [#6876]

---
+++ README.md
@@ -0,0 +1,10 @@
+# Проект для изучения Dependency Injection (DI) 
+
+Этот простой проект создан с целью изучения и понимания Dependency Injection (DI) с использованием Spring Framework.
+
+Dependency Injection (DI) - ключевой концепт в Spring Framework, который помогает достигнуть слабой связанности и поддерживает принцип инверсии управления (IoC). Этот проект служит практическим опытом для понимания DI с использованием Spring.
+
+Используемые технологии:
+* Java
+* Spring Framework (Core, DI)
+* Maven
---
+++ src/main/java/ru/job4j/di/model/StartUI.java
@@ -0,0 +1,24 @@
+package ru.job4j.di.model;
+
+public class StartUI {
+
+    private Store store;
+    private ConsoleInput consoleInput;
+
+    public StartUI(Store store, ConsoleInput consoleInput) {
+        this.store = store;
+        this.consoleInput = consoleInput;
+    }
+
+    public String askInput(String input) {
+        return consoleInput.askStr(input);
+    }
+
+    public void add(String value) {
+        store.add(value);
+    }
+
+    public void print() {
+        store.getAll().forEach(System.out::println);
+    }
+}
