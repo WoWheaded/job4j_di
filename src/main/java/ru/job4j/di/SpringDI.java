c15cd93e4009039ca3438a5c7375654cf71aa41a
WoWheaded <d-343@yandex.ru>
2024-01-08 23:13:23 +0300

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -1,15 +1,16 @@
 package ru.job4j.di;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ru.job4j.di.model.StartUI;
+import ru.job4j.di.model.Store;
 
 public class SpringDI {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.scan("ru.job4j.di.model");
+        context.scan("ru.job4j.di");
         context.refresh();
-        StartUI ui = context.getBean(StartUI.class);
-        ui.add(ui.askInput("Enter name:  "));
-        ui.print();
+        Store store = context.getBean(Store.class);
+        store.add("Petr Arsentev");
+        Store another = context.getBean(Store.class);
+        another.getAll().forEach(System.out::println);
     }
 }
